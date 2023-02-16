408c23e31197c91b79926aba651f62aa2dbf6f25
Favoursimeon <favoursimeon12w@gmail.com>
2023-02-16 12:24:49 -0800

    c program

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/*
+ * main - Entry point
+ *
+ * Description: 'main is the entry point for all c program'
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return(0);
+}
